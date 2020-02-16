fb35da4977380f25657677341cd08eb244b07548
Zami3l <31670103+Zami3l@users.noreply.github.com>
2020-02-16 12:55:19 +0100

    feat(spoofing): ajout affichage et envoi requete

--- scapy/spoofing.py
+++ scapy/spoofing.py
@@ -6,20 +6,32 @@ from pathlib import Path
 import sys, toml, os.path
 
 #Emplacement du fichier de configuration
-pathConfig = Path('scapy\conf.toml')
+pathConfig = Path('./conf.toml')
 
 #Arguments
 if len(sys.argv) == 7:
 
-    result = Ether(src=sys.argv[2], dst=sys.argv[4])/IP(src=sys.argv[1], dst=sys.argv[3])/TCP(dport=sys.argv[5], flags=sys.argv[6])
+    query =     Ether(src=sys.argv[2], dst=sys.argv[4])/ \
+                IP(src=sys.argv[1], dst=sys.argv[3])/ \
+                TCP(dport=sys.argv[5], flags=sys.argv[6])
 
 #Fichier de configuration
 elif os.path.exists(pathConfig):
 
-    config = toml.load(pathConfig)
-    result = Ether(src=config['SRC']['mac'], dst=config['DST']['mac'])/IP(config['SRC']['ip'], dst=config['DST']['ip'])/TCP(dport=config['DST']['port'], flags=config['GEN']['flags'])
+    config = toml.load(pathConfig) 
+
+    query =     Ether(src=config['SRC']['mac'], dst=config['DST']['mac'])/ \
+                IP(src=config['SRC']['ip'], dst=config['DST']['ip'])/ \
+                TCP(dport=config['DST']['port'], flags=config['GEN']['flags'])
 
 #Instruction
 else:
 
     print('Utilisation : py spoofing.py [IP SRC] [MAC SRC] [IP DEST] [MAC DEST] [PORT DEST] [FLAGS]')
+    sys.exit()
+
+print('Requête générée : \n')
+query.show()
+
+#Envoi de la requête
+sendp(query)
